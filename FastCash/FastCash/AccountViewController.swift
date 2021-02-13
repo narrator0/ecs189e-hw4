bf7cb7069478aeba9aaa3f5495abb109434afeff
Huichueh(Jessica) Lo <72643524+jlo0507@users.noreply.github.com>
2021-02-12 17:02:33 -0800

    finish deposit function

--- FastCash/FastCash/AccountViewController.swift
+++ FastCash/FastCash/AccountViewController.swift
@@ -50,6 +50,27 @@ class AccountViewController: UIViewController {
     }
     
     @IBAction func depositButtonPressed(_ sender: Any) {
+
+        let alertController = UIAlertController(title: "Deposit", message: "Enter the amount to deposit", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.keyboardType = .numberPad
+        }
+        let confirmAction = UIAlertAction(title: "Done", style: .default) { [weak alertController] _ in
+            guard let alertController = alertController, let textField = alertController.textFields?.first else { return }
+            print("Current input \(String(describing: textField.text))")
+            guard let unwrapUserInput = textField.text else {return}
+            let depositAmount = Double(unwrapUserInput) ?? 0.0
+            
+            Api.deposit(wallet: self.wallet, toAccountAt: self.accountIndex, amount: depositAmount) { res, err in
+                //let accountVC = self.navigationController?.viewControllers.first as? AccountViewController
+                let homeVC = self.navigationController?.viewControllers.first as? HomeViewController
+                homeVC?.updateWallet(response: res)
+                self.accountAmountLabel.text = self.formatMoney(amount: self.wallet.accounts[self.accountIndex].amount)                //self.navigationController?.pushViewController(accountVC, animated: true)
+            }
+        }
+        alertController.addAction(confirmAction)
+        present(alertController, animated: true, completion: nil)
+
     }
     
     @IBAction func withdrawButtonPressed(_ sender: Any) {
